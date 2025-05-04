649bf1be865c79f7bd97c17f6c80ff6c52c59675
heynecp <heynecp@gmail.com>
2025-05-04 11:56:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ app = FastAPI()
 
 @app.get("/stress/{word}")
 def get_stress(word: str):
+     word = word.lower()
     stresses = pronouncing.stresses(word)
     return {"stress": stresses[0] if stresses else "unknown"}
